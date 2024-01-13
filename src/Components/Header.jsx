f68a1752b7fe56f74a69b25f182537fbf209a7cd
bhanu-100 <bhanusinghx100@gmail.com>
2024-01-13 13:07:21 +0530

    init 2

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
                 <h1><span>FITNESS</span>WITH US</h1>
                 <p className='details'>Build Your Body And Fitness With Professional Touch </p>
                 <div className='header-btns'>
-                 <a href="#" className='header-btn'>JOIN US</a>
+                 <a href="/" className='header-btn'>JOIN US</a>
                 </div>
             </div>
         </div>
--- src/Components/Offer.jsx
+++ src/Components/Offer.jsx
@@ -7,7 +7,7 @@ const Offer = () => {
                 <h1>A BIG <span>OFFER</span> FOR THIS SUMMER</h1>
                 <p className="details">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
             <div className="pr-btns">
-                <a href="#" className='pr-btn'>JOIN NOW</a>
+                <a href="/" className='pr-btn'>JOIN NOW</a>
             </div>
             </div>
         </div>
