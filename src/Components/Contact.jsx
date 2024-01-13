7c94a2ee62eaada48d5321d206a9eae0a894d004
bhanu-100 <bhanusinghx100@gmail.com>
2024-01-13 13:12:02 +0530

    init 4

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
    
     return (
         <div id='contact'>
-            <h1>CONTACT</h1>
+            <h1>CONTACT US</h1>
             <form method='post'>
                 <input  type="text" name='name' placeholder='Full Name' />
                 <input  type="email" name='email' placeholder='Type Your E-Mail' />
